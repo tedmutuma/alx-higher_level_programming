5caf26f5a60460921abb50b74efab1a14f0da29d
Opiomwema <100408799+Opiomwema@users.noreply.github.com>
2022-12-07 23:38:05 +0300

    Update 102-complex_delete.py

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to delete keys with a specific value in a dictionary
+# -----------------------------------------------------------
+
+
+def complex_delete(a_dictionary, value):
+    for idx in list(a_dictionary.keys()):
+        if a_dictionary[idx] == value:
+            del a_dictionary[idx]
+    return a_dictionary

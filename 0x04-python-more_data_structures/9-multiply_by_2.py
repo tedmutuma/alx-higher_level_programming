e1f2da7ed117a4a38d1be57a2e5fd0ac0010e666
Opiomwema <100408799+Opiomwema@users.noreply.github.com>
2022-12-07 23:44:27 +0300

    Update 9-multiply_by_2.py

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to return a new dictionary with all values multiplied by 2
+# -----------------------------------------------------------
+
+
+def multiply_by_2(a_dictionary):
+    new_dict = a_dictionary.copy()
+    for key, value in new_dict.items():
+        new_dict[key] = value * 2
+    return new_dict

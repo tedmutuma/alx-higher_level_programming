c6485464351f1bdfc46f6fa5f49c284f7936912d
Opiomwema <100408799+Opiomwema@users.noreply.github.com>
2022-12-07 23:43:15 +0300

    Update 7-update_dictionary.py

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to replace or add key/value in a dictionary
+# -----------------------------------------------------------
+
+
+def update_dictionary(a_dictionary, key, value):
+    if key not in a_dictionary:
+        a_dictionary[key] = value
+    else:
+        for i in a_dictionary:
+            if i == key:
+                a_dictionary[i] = value
+    return a_dictionary

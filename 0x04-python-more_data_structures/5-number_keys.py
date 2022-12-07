9cb8ab93f8eacfb7209c80ea03c91ed189cb45b2
Opiomwema <100408799+Opiomwema@users.noreply.github.com>
2022-12-07 23:42:10 +0300

    Update 5-number_keys.py

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to return the number of keys in a dictionary
+# -----------------------------------------------------------
+
+
+def number_keys(a_dictionary):
+    return len(a_dictionary.keys())

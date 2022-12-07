ccc00c1dd43690911489e31b0221ed0262fb15ef
Opiomwema <100408799+Opiomwema@users.noreply.github.com>
2022-12-07 23:43:52 +0300

    Update 8-simple_delete.py

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to delete a key in a dictionary
+# -----------------------------------------------------------
+
+
+def simple_delete(a_dictionary, key=""):
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return a_dictionary

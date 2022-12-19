ee93df997744e096e9be0c64b36e71e28ba23615
Opiomwema <100408799+Opiomwema@users.noreply.github.com>
2022-12-19 21:38:11 +0300

    Create 4-list_division.py

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to divide element by element 2 lists
+# demonstrates how to use a try ... except ... finally for exception handling
+#
+# -----------------------------------------------------------
+
+
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    result = 0
+    for index in range(0, list_length):
+        try:
+            result = my_list_1[index] / my_list_2[index]
+        except TypeError:
+            result = 0
+            print("wrong type")
+        except ZeroDivisionError:
+            result = 0
+            print("division by 0")
+        except IndexError:
+            result = 0
+            print("out of range")
+        finally:
+            pass
+        new_list.append(result)
+    return new_list

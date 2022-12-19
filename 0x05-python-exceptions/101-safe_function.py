f1e95f2cd77fe4fc29ecaaef2ad48f95ddc951d1
Opiomwema <100408799+Opiomwema@users.noreply.github.com>
2022-12-19 21:35:11 +0300

    Create 101-safe_function.py

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to execute a function safely
+# demonstrates how to use a try ... except for exception handling
+#
+# -----------------------------------------------------------
+
+
+def safe_function(fct, *args):
+    import sys
+    try:
+        ret = fct(*args)
+        return ret
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return None

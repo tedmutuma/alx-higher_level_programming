e27203e397082cc6a7d107c2c73e94b8a503a1d9
Opiomwema <100408799+Opiomwema@users.noreply.github.com>
2022-12-19 21:38:45 +0300

    Create 5-raise_exception.py

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to raise a type exception
+# demonstrates how to raise exception
+#
+# -----------------------------------------------------------
+
+
+def raise_exception():
+    raise TypeError

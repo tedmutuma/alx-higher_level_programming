0f6c2d1bbdb49451df2cd0dc5b497b3349450f3b
Opiomwema <100408799+Opiomwema@users.noreply.github.com>
2022-12-19 21:39:26 +0300

    Create 6-raise_exception_msg.py

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to raise a name exception with a message
+# demonstrates how to raise exception
+#
+# -----------------------------------------------------------
+
+
+def raise_exception_msg(message=""):
+    raise NameError(message)

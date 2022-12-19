78271b5cb6eb5bbb9acdbc65b82f3b0cd66f8d02
Opiomwema <100408799+Opiomwema@users.noreply.github.com>
2022-12-19 21:33:35 +0300

    Create 1-safe_print_integer.py

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to print an integer with "{:d}".format()
+# demonstrates how to use a try ... except for exception handling
+#
+# -----------------------------------------------------------
+
+
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+    except (TypeError, ValueError):
+        return False
+
+    return True

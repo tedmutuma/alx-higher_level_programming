f613986b08c5db77045b472397877c561af5301c
Opiomwema <100408799+Opiomwema@users.noreply.github.com>
2022-12-19 21:37:02 +0300

    Create 2-safe_print_list_integers.py

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to print the first x elements of a list and only integers
+# demonstrates how to use a try ... except for exception handling
+#
+# -----------------------------------------------------------
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    num_of_elements = 0
+    for index in range(x):
+        try:
+            print("{:d}".format(my_list[index]), end="")
+            num_of_elements += 1
+        except (ValueError, TypeError):
+            pass
+
+    print()
+    return num_of_elements

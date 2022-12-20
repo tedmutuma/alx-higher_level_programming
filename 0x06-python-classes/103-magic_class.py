3cce90bf7995894f529084cb816f19fdfab5ec9c
Opiomwema <100408799+Opiomwema@users.noreply.github.com>
2022-12-20 17:32:03 +0300

    Create 103-magic_class.py

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+# -----------------------------------------------------------
+# -----------------------------------------------------------
+"""MagicClass.
+This module contains code representing a circle.
+Usage Example:
+    MagicClass = __import__('103-magic_class').MagicClass
+    a_circle = MagicClass(5)
+    print(a_circle.area())
+"""
+import math
+
+
+class MagicClass:
+    """Represent a circle."""
+
+    def __init__(self, radius=0):
+        """Initialize a MagicClass.
+        Arg:
+            radius (float or int): The radius of the new MagicClass.
+        """
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
+
+    def area(self):
+        """Return the area of the MagicClass."""
+        return self.__radius**2 * math.pi
+
+    def circumference(self):
+        """Return The circumference of the MagicClass."""
+        return 2 * math.pi * self.__radius

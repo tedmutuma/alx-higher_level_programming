2a6cf7d99770f4cc217f06279b40c4d44414df0d
Opiomwema <100408799+Opiomwema@users.noreply.github.com>
2022-12-20 17:31:24 +0300

    Create 102-square.py

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+# -----------------------------------------------------------
+# -----------------------------------------------------------
+"""Square Class.
+This module contains a class that defines a square.
+Usage Example:
+    Square = __import__('102-square').Square
+    s_5 = Square(5)
+    s_6 = Square(6)
+    if s_5 < s_6:
+        print("Square 5 < Square 6")
+    if s_5 <= s_6:
+        print("Square 5 <= Square 6")
+"""
+
+
+class Square:
+    """Defines the blueprint of a square.
+    Attribute:
+        size (int): An integer representing the object size.
+    """
+
+    def __init__(self, size=0):
+        """An object constructor method."""
+        self.__size = size
+
+    @property
+    def size(self):
+        """Gets the size private attribute value.
+        Returns:
+            The size private attribute
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size private attribute value.
+        Validates the assignment of the size private attribute.
+        Arg:
+            value: the value to be set
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """A public object method.
+        Returns:
+            The current square area
+        """
+        return self.__size**2
+
+    def __eq__(self, o):
+        """Defines the == comparison"""
+        return self.__size == o.__size
+
+    def __ne__(self, o):
+        """Defines the != comparison"""
+        return self.__size != o.__size
+
+    def __gt__(self, o):
+        """Defined the > comparison"""
+        return self.__size > o.__size
+
+    def __ge__(self, o):
+        """Defined the >= comparison"""
+        return self.__size >= o.__size
+
+    def __lt__(self, o):
+        """Defined the < comparison"""
+        return self.__size < o.__size
+
+    def __le__(self, o):
+        """Defined the <= comparison"""
+        return self.__size <= o.__size

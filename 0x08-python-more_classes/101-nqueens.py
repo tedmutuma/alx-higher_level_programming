6701bb76292924e57faab8e0eef4220df3932578
Opiomwema <100408799+Opiomwema@users.noreply.github.com>
2023-01-04 21:49:08 +0300

    Update 101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-"""
-nqueens backtracking program to print the coordinates of n queens
-on an nxn grid such that they are all in non-attacking positions
-"""
 from sys import argv
 if __name__ == "__main__":
     a = []

14e50b80243720fabe62e498d4cfe3fa0b605ea2
Opiomwema <100408799+Opiomwema@users.noreply.github.com>
2023-03-24 10:49:31 +0300

    Update 4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """
-This script  takes in the name of a state
-as an argument and lists all cities of that
-state, using the database `hbtn_0e_4_usa`.
+This script lists all cities from
+the database `hbtn_0e_4_usa`.
 """
 
 import MySQLdb
@@ -20,22 +19,19 @@ if __name__ == '__main__':
     with db.cursor() as cur:
         cur.execute("""
             SELECT
-                cities.id, cities.name
+                cities.id, cities.name, states.name
             FROM
                 cities
             JOIN
                 states
             ON
                 cities.state_id = states.id
-            WHERE
-                states.name LIKE BINARY %(state_name)s
             ORDER BY
                 cities.id ASC
-        """, {
-            'state_name': argv[4]
-        })
+        """)
 
         rows = cur.fetchall()
 
     if rows is not None:
-        print(", ".join([row[1] for row in rows]))
+        for row in rows:
+            print(row)

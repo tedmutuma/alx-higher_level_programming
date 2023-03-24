a6a9389d820015e8e329f209e70c6df1799f22b7
Opiomwema <100408799+Opiomwema@users.noreply.github.com>
2023-03-24 10:51:09 +0300

    Update 9-model_state_filter_a.py

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
-"""List all State objects containing `a` from db"""
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+"""
+This script lists all State objects
+that contain the letter `a`
+from the database `hbtn_0e_6_usa`.
+"""
+
+from sys import argv
 from model_state import Base, State
-
-
-def list_a_state_obj():
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
-    session = Session(engine)
-
-    rows = session.query(State).all()
-
-    for i in rows:
-        if 'a' in i.__dict__['name']:
-            print("{}: {}".format(i.__dict__['id'], i.__dict__['name']))
-
-    session.close()
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    list_a_state_obj()
+    """
+    Access to the database and get a state
+    from the database.
+    """
+
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+    engine = create_engine(db_uri)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    for instance in session.query(State).filter(State.name.contains('a')):
+        print('{0}: {1}'.format(instance.id, instance.name))

f796e10877015d612fe01fa120dd0ba327541b63
Opiomwema <100408799+Opiomwema@users.noreply.github.com>
2023-03-24 10:45:56 +0300

    Update 14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
-"""Displays all City obj from db"""
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+"""
+This script prints all City objects
+from the database `hbtn_0e_14_usa`.
+"""
+
+from sys import argv
 from model_state import Base, State
 from model_city import City
-
-
-def list_city_obj():
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
-    session = Session(engine)
-
-    rows = session.query(State, City).join(City).all()
-
-    for i in rows:
-        print("{}: ({}) {}".format(i[0].__dict__['name'],
-                                   i[1].__dict__['id'],
-                                   i[1].__dict__['name']))
-
-    session.close()
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    list_city_obj()
+    """
+    Access to the database and get the cities
+    from the database.
+    """
+
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+    engine = create_engine(db_uri)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    query = session.query(City, State).join(State)
+
+    for _c, _s in query.all():
+        print("{}: ({:d}) {}".format(_s.name, _c.id, _c.name))
+
+    session.commit()
+    session.close()
